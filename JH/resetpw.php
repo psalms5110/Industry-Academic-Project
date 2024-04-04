13b9457e5d761ae3e261b348196eb102559cb018
Hun133 <wjdgns12345678@gmail.com>
2024-04-05 01:53:49 +0900

    asd

---
+++ JH/resetpw.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>PW 재설정</title>
+    <link href="css/resetpw.css" rel="stylesheet">
+  </head>
+  <body>
+    <form action="resetpw.php" method="POST">
+      <div class="container">
+        <h1>PW 재설정</h1>
+        <p><input type="password" id="current_password" name="current_password" placeholder="현재 비밀번호" required></p>
+        
+          <div class="new_password">
+            <p><input type="password" id="new_password" name="new_password" placeholder="새 비밀번호" required></p>
+            <p><input type="password" id="new_password_confirm" name="new_password_confirm" placeholder="새 비밀번호 확인" required></p>
+            <p><input type="submit" value="비밀번호 변경"></p>
+          </div>
+    </form>
+</div>
+  </body>
+</html>
