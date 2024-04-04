c56ac58aec2cf5664309b121bf5c829bcfe24749
psalms5110 <psalms51100@gmail.com>
2024-04-04 22:33:18 +0900

    up

--- L/TestFA/index.php
+++ L/TestFA/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>ARES</title>
-    <link href="css/index.css" rel="stylesheet">
+    <link href="CSS/index.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
