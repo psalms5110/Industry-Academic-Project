7fb290ab4040d2428cd3ec62f1e64f491065c137
0cupid <144975721+0cupid@users.noreply.github.com>
2024-06-12 20:54:50 +0900

    Add files via upload

--- H/Sec News Writing/index.php
+++ H/Sec News Writing/index.php
@@ -4,45 +4,48 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>ARES</title>
+  <title>post</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
   <header>
-    <!-- 이미지 -->
-    <img class="cate" src="cate.jpg" alt="cate">
-    <img class="home-image" src="home.jpg" alt="home">
-
-    <!-- 검색창 -->
-    <div class="search-container">
-      <input type="text" placeholder="검색하세요.">
-      <button type="submit">검색</button>
-      <img class="profile-image" src="your-image.jpg" alt="Profile Image">
+    <div class="header-container">
+      <div class="logo-container">
+        <img class="cate" src="cate.jpg" alt="cate">
+        <img class="home-image" src="home.jpg" alt="home">
+      </div>
+      <div class="search-container">
+        <input type="text" placeholder="검색하세요.">
+        <button type="submit">검색</button>
+        <img class="profile-image" src="your-image.jpg" alt="Profile Image">
+      </div>
     </div>
   </header>
-  <div id="board_write">
-    <div id="write_area">
-      <form enctype="multipart/form-data" action="write_ok.php?board_id=<?echo $board_id;?>" method="post">
-        <div id="in_title">
-          <textarea name="title" id="utitle" rows="1" cols="55" placeholder="제목" maxlength="100" required></textarea>
-        </div>
+  <div class="note" style="margin-top:50px;">
+    <form>
+      <label for="title">제목</label>
+      <input type="text" id="title" name="title" required>
 
-        <div class="wi_line"></div>
-        <div id="in_content">
-          <textarea name="content" id="ucontent" placeholder="내용" required></textarea>
-        </div>
+      <label for="title">부제목</label>
+      <input type="text" id="subtitle" name="subtitle" required>
 
-        <input type="file" name="SelectFile" />
+      <label for="content">내용</label>
+      <textarea id="content" name="content" rows="10" required></textarea>
+
+      <label for="attachment">첨부 파일:</label>
+      <input type="file" id="attachment" name="attachment">
+
+      <label for="content">출처</label>
+      <textarea id="from" name="content" rows="2" required></textarea>
 
 
-        <div class="bt_se">
-          <button type="submit">저장</button>
-        </div>
-      </form>
-    </div>
   </div>
-  <script src="script.js"></script>
+  <div class="save">
+    <button type="submit">저장</button>
+  </div>
+
+  </form>
 </body>
 
 </html>
