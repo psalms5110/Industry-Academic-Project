2d62e5653ec6946c1ca69237a479060438405b98
0cupid <144975721+0cupid@users.noreply.github.com>
2024-06-12 21:07:06 +0900

    Add files via upload

--- H/Post(Free board)/index.php
+++ H/Post(Free board)/index.php
@@ -2,67 +2,160 @@
 <html lang="ko">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>post</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>post</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-  <header>
-    <!-- 이미지 -->
-    <img class="cate" src="cate.jpg" alt="cate">
-    <img class="home-image" src="home.jpg" alt="home">
-
-    <!-- 검색창 -->
-    <div class="search-container">
-      <input type="text" placeholder="검색하세요.">
-      <button type="submit">검색</button>
-      <img class="profile-image" src="your-image.jpg" alt="Profile Image">
+    <header>
+        <div class="header-container">
+            <div class="logo-container">
+                <img class="cate" src="cate.jpg" alt="cate">
+                <img class="home-image" src="home.jpg" alt="home">
+            </div>
+            <div class="search-container">
+                <input type="text" placeholder="검색하세요.">
+                <button type="submit">검색</button>
+                <img class="profile-image" src="your-image.jpg" alt="Profile Image">
+            </div>
+        </div>
+    </header>
+    <div style="text-align: center; color: #656565;">
+        <h1>[ 자유게시판 ]</h1>
     </div>
-  </header>
-  <!-- 내용 -->
-  <h1>[ 자유게시판 ]</h1>
-  <section>
-    <article>
-        <div class="main1">
-            <img class="img1" src="main1.jpg" alt="main1">
-            <p>hello world! hello world! hello world! hello world! hello world! hello world! hello world! 
-                hello world! hello world! hello world! hello world! hello world! hello world! hello world! 
-            </p>
-        </div>
-        <div class="main2">
-            <h3> < 내용 ></h3>
-            <p> whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
-                whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
-            </p>
-            <img class="img2" src="main2.jpg" alt="main2">
-            <ol>
-                <li>세부내용</li>
-                <p> whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
-                    whatrudoing whatrudoing whatrudoing
-                </p>
-                <li>세부내용2</li>
-                <p> whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
-                    whatrudoing whatrudoing whatrudoing
-                </p>
-            </ol>
-        </div>
-    </article>
+    <table>
+        <tr>
+            <th style="background-color: #f2f2f2;">닉네임</th>
+            <th>ARES</th>
+            <th style="background-color: #f2f2f2;">등록일</th>
+            <th>20xx.xx.xx</th>
+            <th style="background-color: #f2f2f2;">추천수</th>
+            <th>n</th>
+        </tr>
+        <tr>
+            <td style="background-color: #f2f2f2;">제목</td>
+            <td colspan="5" style="background-color: white;">자유게시판 제목</td>
+        </tr>
+    </table>
+
+
+    <img class="img1" src="main1.jpg" alt="main1">
+    <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+
+    <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+    <hr style="margin-top: 80px;">
+
     <div class="down-buttons">
         <button><img src="추천.jpg" alt="thumbsup"> 추천</button>
         <button><img src="저장.jpg" alt="save">저장</button>
     </div>
-    <div class="container">
-    <div id="comment">
-        <div id="comment-count">댓글 <span id="count">0</span></div>
-        <input id="comment-input" placeholder="댓글을 입력해주세요.">
-        <button id="submit">작성</button>
+    </article>
+    </section>
+    <div class="reply">
+        <div class="comment-input">
+            <div class="comment-input-field">
+                <textarea placeholder=""></textarea>
+                <button class="comment-submit-btn">작성</button>
+            </div>
+        </div>
+        <div class="comment-item">
+            <div class="re-profile-image-container">
+                <img class="re-profile-image" src="your-image.jpg" alt="Profile">
+            </div>
+            <div class="comment-content">
+                <div class="username">닉네임</div>
+                <div class="comment-actions">
+                    <a href="#">수정</a> | <a href="#">삭제</a>
+                </div>
+                <div class="text">잘 보고 갑니다.</div>
+                <div class="timestamp-container">
+                    <div class="timestamp">2023-05-01 12:34</div>
+                </div>
+            </div>
+        </div>
+
+        <hr style="margin-top:20px;">
+        <div class="comment-item">
+            <div class="re-profile-image-container">
+                <img class="re-profile-image" src="your-image.jpg" alt="Profile">
+            </div>
+            <div class="comment-content">
+                <div class="username">닉네임</div>
+                <div class="comment-actions">
+                    <a href="#">수정</a> | <a href="#">삭제</a>
+                </div>
+                <div class="text">잘 보고 갑니다.</div>
+                <div class="timestamp-container">
+                    <div class="timestamp">2023-05-02 12:34</div>
+                </div>
+            </div>
+        </div>
+        <hr style="margin-top:20px;">
+        <div class="comment-item">
+            <div class="re-profile-image-container">
+                <img class="re-profile-image" src="your-image.jpg" alt="Profile">
+            </div>
+            <div class="comment-content">
+                <div class="username">닉네임</div>
+                <div class="comment-actions">
+                    <a href="#">수정</a> | <a href="#">삭제</a>
+                </div>
+                <div class="text">잘 보고 갑니다.</div>
+                <div class="timestamp-container">
+                    <div class="timestamp">2023-05-03 12:34</div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="comment-item">
+            <div class="re-profile-image-container">
+                <img class="re-profile-image" src="your-image.jpg" alt="Profile">
+            </div>
+            <div class="comment-content">
+                <div class="username">닉네임</div>
+                <div class="comment-actions">
+                    <a href="#">수정</a> | <a href="#">삭제</a>
+                </div>
+                <div class="text">잘 보고 갑니다.</div>
+                <div class="timestamp-container">
+                    <div class="timestamp">2023-05-02 12:34</div>
+                </div>
+                <p style="text-align: left;">댓글 (1개)</p>
+                <hr>
+                <div class="reply-container">
+                    <div class="comment-item">
+                        <div class="re-profile-image-container">
+                            <img class="re-profile-image" src="another-image.jpg" alt="Profile">
+                        </div>
+                        <div class="comment-content">
+                            <div class="username">답글 작성자</div>
+                            <div class="comment-actions">
+                                <a href="#">수정</a> | <a href="#">삭제</a>
+                            </div>
+                            <div class="text">감사합니다.</div>
+                            <div class="timestamp-container">
+                                <div class="timestamp">2023-05-03 10:15</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-    </div>
-  </section>
-  <footer></footer>
-  <script src="script.js"></script>
+
 </body>
 
 </html>
