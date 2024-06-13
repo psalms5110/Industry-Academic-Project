980c4693e166ddbab85d94caa01201a561aecf77
0cupid <144975721+0cupid@users.noreply.github.com>
2024-06-13 16:47:47 +0900

    Add files via upload

---
+++ H/Usermanagement/index.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ares</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <div class="logo-container">
+                <img class="cate" src="cate.png" alt="cate">
+                <img class="home-image" src="home.png" alt="home">
+            </div>
+            <div class="sidebar-content">
+                <ul>
+                    <a href="#" class="not-link">
+                        <h2>DashBoard</h2>
+                    </a>
+                    <a href="#" class="not-link">
+                        <h2>User Management</h2>
+                    </a>
+                </ul>
+            </div>
+        </div>
+        <div class="main-content"><img class="profile-image" src="profile.png" alt="Profile Image">
+            <div class="table-of-contents">
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+
+                <hr>
+
+                <button class="toc-btn">
+                    <div class="re-profile-image-container">
+                        <img class="re-profile-image" src="profile.png" alt="Profile">
+                        <div class="username">아이디<br />닉네임</div>
+                    </div>
+                </button>
+            </div>
+            <div class="right-container">
+                <div class="top-container">
+                    <div class="top-left-container">
+                        <div class="profile-container1">
+                            <img class="re-profile-image1" src="profile.png" alt="Profile">
+                            <div class="username-container1">
+                                <div class="username1">닉네임</div>
+                                <div class="user-id1">아이디</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="top-right-container">
+                        <!-- 상단 오른쪽 카드 -->
+                        <div class="stats-card">
+                            <div class="stats-item">
+                                <div class="label">누적 방문 수</div>
+                                <div class="value">8</div>
+                            </div>
+                            <div class="stats-item">
+                                <div class="label">올린 게시글 수</div>
+                                <div class="value">3</div>
+                            </div>
+                            <div class="stats-item">
+                                <div class="label">단 댓글 수</div>
+                                <div class="value">10</div>
+                            </div>
+                            <div class="stats-item">
+                                <div class="label">누른 추천 수</div>
+                                <div class="value">10</div>
+                            </div>
+                            <div class="stats-item">
+                                <div class="label">받은 추천 수</div>
+                                <div class="value">12</div>
+                            </div>
+                            <div class="stats-item">
+                                <div class="label">북마크 수</div>
+                                <div class="value">12</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom-container">
+                    <div class="bottom-left-container">
+                            <div class="table-of-contents2">
+                                <div class="comment-header">
+                                <p><b>작성한 댓글</b></p>
+                                <button class="rem-comment-btn">삭제</button></div>
+                            <div class="comment-item">
+                                <div class="checks">
+                                    <label for="check">
+                                        <input type="checkbox" id="save-id" name="save-id" value="true">
+                                    </label>
+                                </div>
+                                <div class="comment-content">
+                                    <div class="cate3">카테고리/게시판</div>
+                                </div>
+                                <div class="text">내용</div>
+                                <div class="timestamp-container">
+                                    <div class="timestamp">2023-05-01 12:34</div>
+                                </div>
+                            </div>
+                            <div class="comment-item">
+                                <div class="checks">
+                                    <label for="check">
+                                        <input type="checkbox" id="save-id" name="save-id" value="true">
+                                    </label>
+                                </div>
+                                <div class="comment-content">
+                                    <div class="cate3">카테고리/게시판</div>
+                                </div>
+                                <div class="text">내용</div>
+                                <div class="timestamp-container">
+                                    <div class="timestamp">2023-05-01 12:34</div>
+                                </div>
+                            </div>
+                            <div class="comment-item">
+                                <div class="checks">
+                                    <label for="check">
+                                        <input type="checkbox" id="save-id" name="save-id" value="true">
+                                    </label>
+                                </div>
+                                <div class="comment-content">
+                                    <div class="cate3">카테고리/게시판</div>
+                                </div>
+                                <div class="text">내용</div>
+                                <div class="timestamp-container">
+                                    <div class="timestamp">2023-05-01 12:34</div>
+                                </div>
+                            </div>
+                            <div class="comment-item">
+                                <div class="checks">
+                                    <label for="check">
+                                        <input type="checkbox" id="save-id" name="save-id" value="true">
+                                    </label>
+                                </div>
+                                <div class="comment-content">
+                                    <div class="cate3">네트워크 보안</div>
+                                </div>
+                                <div class="text">내용</div>
+                                <div class="timestamp-container">
+                                    <div class="timestamp">2023-05-01 12:34</div>
+                                </div>
+                            </div>
+                            <div class="comment-item">
+                                <div class="checks">
+                                    <label for="check">
+                                        <input type="checkbox" id="save-id" name="save-id" value="true">
+                                    </label>
+                                </div>
+                                <div class="comment-content">
+                                    <div class="cate3">카테고리/게시판</div>
+                                </div>
+                                <div class="text">내용</div>
+                                <div class="timestamp-container">
+                                    <div class="timestamp">2023-05-01 12:34</div>
+                                </div>
+                            </div>
+                            <div class="comment-item">
+                                <div class="checks">
+                                    <label for="check">
+                                        <input type="checkbox" id="save-id" name="save-id" value="true">
+                                    </label>
+                                </div>
+                                <div class="comment-content">
+                                    <div class="cate3">카테고리/게시판</div>
+                                </div>
+                                <div class="text">내용</div>
+                                <div class="timestamp-container">
+                                    <div class="timestamp">2023-05-01 12:34</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="bottom-right-container">
+                            <div class="table-of-contents2">
+                                <div class="comment-header">
+                                <p><b>작성한 댓글</b></p>
+                                <button class="rem-comment-btn">삭제</button></div>
+                                <div class="comment-item">
+                                    <div class="checks">
+                                        <label for="check">
+                                            <input type="checkbox" id="save-id" name="save-id" value="true">
+                                        </label>
+                                    </div>
+                                    <div class="comment-content">
+                                        <div class="cate3">카테고리/게시판</div>
+                                    </div>
+                                    <div class="text">잘 보고 갑니다.</div>
+                                    <div class="timestamp-container">
+                                        <div class="timestamp">2023-05-01 12:34</div>
+                                    </div>
+                                </div>
+                                <div class="comment-item">
+                                    <div class="checks">
+                                        <label for="check">
+                                            <input type="checkbox" id="save-id" name="save-id" value="true">
+                                        </label>
+                                    </div>
+                                    <div class="comment-content">
+                                        <div class="cate3">카테고리/게시판</div>
+                                    </div>
+                                    <div class="text">잘 보고 갑니다.</div>
+                                    <div class="timestamp-container">
+                                        <div class="timestamp">2023-05-01 12:34</div>
+                                    </div>
+                                </div>
+                                <div class="comment-item">
+                                    <div class="checks">
+                                        <label for="check">
+                                            <input type="checkbox" id="save-id" name="save-id" value="true">
+                                        </label>
+                                    </div>
+                                    <div class="comment-content">
+                                        <div class="cate3">카테고리/게시판</div>
+                                    </div>
+                                    <div class="text">잘 보고 갑니다.</div>
+                                    <div class="timestamp-container">
+                                        <div class="timestamp">2023-05-01 12:34</div>
+                                    </div>
+                                </div>
+                                <div class="comment-item">
+                                    <div class="checks">
+                                        <label for="check">
+                                            <input type="checkbox" id="save-id" name="save-id" value="true">
+                                        </label>
+                                    </div>
+                                    <div class="comment-content">
+                                        <div class="cate3">카테고리/게시판</div>
+                                    </div>
+                                    <div class="text">잘 보고 갑니다.</div>
+                                    <div class="timestamp-container">
+                                        <div class="timestamp">2023-05-01 12:34</div>
+                                    </div>
+                                </div>
+                                <div class="comment-item">
+                                    <div class="checks">
+                                        <label for="check">
+                                            <input type="checkbox" id="save-id" name="save-id" value="true">
+                                        </label>
+                                    </div>
+                                    <div class="comment-content">
+                                        <div class="cate3">카테고리/게시판</div>
+                                    </div>
+                                    <div class="text">잘 보고 갑니다.</div>
+                                    <div class="timestamp-container">
+                                        <div class="timestamp">2023-05-01 12:34</div>
+                                    </div>
+                                </div>
+                                <div class="comment-item">
+                                    <div class="checks">
+                                        <label for="check">
+                                            <input type="checkbox" id="save-id" name="save-id" value="true">
+                                        </label>
+                                    </div>
+                                    <div class="comment-content">
+                                        <div class="cate3">카테고리/게시판</div>
+                                    </div>
+                                    <div class="text">잘 보고 갑니다.</div>
+                                    <div class="timestamp-container">
+                                        <div class="timestamp">2023-05-01 12:34</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
