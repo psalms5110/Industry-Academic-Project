debb1ac830585bfff8a236968074ed375e9bb65f
0cupid <144975721+0cupid@users.noreply.github.com>
2024-06-13 16:34:39 +0900

    Add files via upload

--- H/Sec News Writing/index.php
+++ H/Sec News Writing/index.php
@@ -12,32 +12,32 @@
   <header>
     <div class="header-container">
       <div class="logo-container">
-        <img class="cate" src="cate.jpg" alt="cate">
-        <img class="home-image" src="home.jpg" alt="home">
+        <img class="cate" src="cate.png" alt="cate">
+        <img class="home-image" src="home.png" alt="home">
       </div>
       <div class="search-container">
         <input type="text" placeholder="검색하세요.">
         <button type="submit">검색</button>
-        <img class="profile-image" src="your-image.jpg" alt="Profile Image">
+        <img class="profile-image" src="profile.png" alt="Profile Image">
       </div>
     </div>
   </header>
-  <div class="note" style="margin-top:50px;">
+  <div class="note" style="margin-top:50px; padding:20px;">
     <form>
-      <label for="title">제목</label>
-      <input type="text" id="title" name="title" required>
+      <label for="title" style="color:#656565;">제목</label>
+      <input type="text" style="border: 1px solid #C6C6C6;" id="title" name="title" required>
 
-      <label for="title">부제목</label>
-      <input type="text" id="subtitle" name="subtitle" required>
+      <label for="title" style="color:#656565;">부제목</label>
+      <input type="text" style="border: 1px solid #C6C6C6;" id="subtitle" name="subtitle" required>
 
-      <label for="content">내용</label>
-      <textarea id="content" name="content" rows="10" required></textarea>
+      <label for="content" style="color:#656565;">내용</label>
+      <textarea id="content" style="border: 1px solid #C6C6C6;" name="content" rows="10" required></textarea>
 
-      <label for="attachment">첨부 파일:</label>
+      <label for="attachment" style="color:#656565;"">첨부 파일:</label>
       <input type="file" id="attachment" name="attachment">
 
-      <label for="content">출처</label>
-      <textarea id="from" name="content" rows="2" required></textarea>
+      <label for="content" style="color:#656565;">출처</label>
+      <textarea id="from" style="border: 1px solid #C6C6C6;" name="content" rows="2" required></textarea>
 
 
   </div>
