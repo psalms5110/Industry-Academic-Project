3019ba2a9971efa05d67515eba847b4b668e2b87
0cupid <144975721+0cupid@users.noreply.github.com>
2024-06-13 16:43:46 +0900

    Add files via upload

--- H/SecNews/index.php
+++ H/SecNews/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>post</title>
+  <title>Ares</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -12,13 +12,13 @@
   <header>
     <div class="header-container">
       <div class="logo-container">
-        <img class="cate" src="cate.jpg" alt="cate">
-        <img class="home-image" src="home.jpg" alt="home">
+        <img class="cate" src="cate.png" alt="cate">
+        <img class="home-image" src="home.png" alt="home">
       </div>
       <div class="search-container">
         <input type="text" placeholder="검색하세요.">
         <button type="submit">검색</button>
-        <img class="profile-image" src="your-image.jpg" alt="Profile Image">
+        <img class="profile-image" src="profile.png" alt="Profile Image">
       </div>
     </div>
   </header>
@@ -34,7 +34,7 @@
       </div>
       <hr>
       <h3 style="color:#656565;">보안 뉴스 부제목</h3>
-      <img class="img1" src="main1.jpg" alt="main1">
+      <img class="img1" style="color:#656565;"" src="main1.png" alt="main1">
       <p>whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
         whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing</p>
       <p>whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
@@ -42,14 +42,12 @@
       <p>whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing
         whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing whatrudoing</p>
       <div class="down-buttons">
-        <button><img src="추천.jpg" alt="thumbsup"> 추천</button>
-        <button><img src="저장.jpg" alt="save">저장</button>
+        <button><img class="thumb" src="thumbup.png" alt="thumbsup"> 추천</button>
+        <button><img class="save" src="save.png" alt="save">저장</button>
       </div>
     </article>
   </section>
 
-  <footer></footer>
-  <script src="script.js"></script>
 </body>
 
 </html>
